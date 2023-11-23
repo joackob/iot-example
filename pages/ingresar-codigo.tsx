ace43ba9e0f18702f628faad1b7cb91c3192dff5
diaz-11 <angiediazvilca@gmail.com>
2023-11-23 20:00:38 -0300

    soy liz

--- pages/ingresar-codigo.tsx
+++ pages/ingresar-codigo.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Modal, Skeleton, Stack, Typography } from "@mui/material";
+import { Box, Button, Modal, Skeleton, Stack, TextField, Typography } from "@mui/material";
 import { useState } from "react";
 
 const IngresarCodigo = () => {
@@ -14,12 +14,18 @@ const IngresarCodigo = () => {
       <Modal open={open} onClose={handleClose}>
         <Stack alignItems="center" justifyContent={"center"} height={"100%"}>
           <Box
-            sx={{ backgroundColor: "white", width: "480px", height: "480px", paddingTop:"50px"}}
+            sx={{ backgroundColor: "white", width: "480px", height: "480px", paddingTop: "50px", borderRadius: "30px", paddingRight: "100px", paddingLeft: "100px" }}
+
           >
             <Typography variant="h5" textAlign={"center"}>Clave de desbloqueo</Typography>
             <Typography variant="h6" textAlign={"center"}>
               Ingrese la secuencia de letras que aparecen debajo del codigo QR
             </Typography>
+            <TextField id="outlined-basic" label="C.1" variant="outlined" sx={{ width: "50px", height: "100px" }} />
+            <TextField id="outlined-basic" label="C.2" variant="outlined" sx={{ width: "50px", height: "100px" }} />
+            <TextField id="outlined-basic" label="C.3" variant="outlined" sx={{ width: "50px", height: "100px" }} />
+            <TextField id="outlined-basic" label="C.4" variant="outlined" sx={{ width: "50px", height: "100px" }} />
+            <TextField id="outlined-basic" label="C.5" variant="outlined" sx={{ width: "50px", height: "100px" }} />
           </Box>
         </Stack>
       </Modal>
