85bc283098b90240875f3f4f516185d565b42e4e
Liss <kiyoomis96@gmail.com>
2023-12-05 18:55:59 -0300

    deje un ping to test

--- pages/api/ping.ts
+++ pages/api/ping.ts
@@ -1,8 +1,25 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import mqtt from "mqtt/*";
+import {config} from "../../config"
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
+    const client = mqtt.connect("mqtt://test.mosquitto.org");
+
+    client.on("connect", () => {
+        client.subscribe("presence", (err) => {
+            if (!err) {
+                client.publish("presence", "Hello mqtt");
+            }
+        });
+    });
+
+    client.on("message", (topic, message) => {
+        // message is Buffer
+        console.log(message.toString());
+        client.end();
+    });
     res.status(200).json({ ping: "pong" });
 }
