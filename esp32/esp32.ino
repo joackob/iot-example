b66941f23c9ceadc12a056f337d2716984d24aff
Joaquin Blanco <blancojoaquin77@gmail>
2023-12-23 15:28:54 -0300

    se agregaron configuraciones a esp32 pero no se asegura que los mismos funciones como se espera

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -2,33 +2,20 @@
 Rui Santos
 Complete project details at https://randomnerdtutorials.com
 *********/
+
 // bibliotecas
 #include <WiFi.h>
 #include <PubSubClient.h> //Nick O'Leary
 #include <ESP32Servo.h>   //Kevin Harrington,John K. Bennett
 
-// Averiguar si estamos en modo AP o STA
-//  Reemplace las siguientes variables con su combinación SSID/Contraseña
-const char *ssid = "ETEC-UBA";
-//"Carlos";
-//"ETEC-UBA";
-const char *password = "ETEC-alumnos@UBA";
-//"arduino95";
-//"ETEC-alumnos@UBA";
+// Settings
+#include "wifi_config.h"
+#include "mqtt_config.h"
 
-// Add your MQTT Broker IP address, example:
-// Agregue su dirección IP de MQTT Broker(ip donde esta el mosquito), ejemplo:
-// const char* mqtt_server = "192.168.1.144";
-const char *mqtt_server = "10.9.120.141"; // cambiamos la ip a la del servidor
-//"192.168.224.128"; yo
-//"192.168.0.138"; equipo
-// 10.9.121.216 (11 dic)
-// 10.9.121.73 ()
+// Averiguar si estamos en modo AP o STA
+
 WiFiClient espClient;               // para controlar la conexión de WiFi
 PubSubClient mqttClient(espClient); // para controlar la conexión de MQTT
-long lastMsg = 0;                   // como si fuese un int
-char msg[50];                       // cpomo si fuese un String
-int value = 0;
 
 // LED Pin
 const int ledPin = 2;
@@ -41,18 +28,19 @@ void setup()
   pinMode(ledPin, OUTPUT);
   barrera.attach(23);
   setup_wifi();
-  mqttClient.setServer(mqtt_server, 1883); // puerto (1883)
-  mqttClient.setCallback(callback);        // le digo a qué función del código debe llamar, cuando el servidor nos envía un mensaje
+  mqttClient.setServer(MQTT_HOST, 1883); // puerto (1883)
+  mqttClient.setCallback(callback);      // le digo a qué función del código debe llamar, cuando el servidor nos envía un mensaje
 }
 
 void setup_wifi()
-{ // muesta lo que pasa en el monitor serie
+{
+  // muesta lo que pasa en el monitor serie
   delay(10);
   // empezamos conectandolo a una red wifi
   Serial.println();
   Serial.print("Connecting to "); // se esta conectando a cliente y el servidor
   Serial.println(ssid);
-  WiFi.begin(ssid, password);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
 
   while (WiFi.status() != WL_CONNECTED)
   {
@@ -76,7 +64,7 @@ void callback(char *topic, byte *message, unsigned int length)
   Serial.print("Message arrived on topic: ");
   Serial.print(topic);
   Serial.print(". Message: ");
-  String Led;
+  String Led(message);
   for (int i = 0; i < length; i++)
   {
     Serial.print((char)message[i]);
@@ -91,19 +79,17 @@ void callback(char *topic, byte *message, unsigned int length)
 
   // Changes the output state according to the message
   // Cambia el estado de salida según el mensaje.
-  if (String(topic) == "Carlosyliz")
+  if (String(topic) == MQTT_TOPIC)
   {
     Serial.print("Cambiar la salida a");
-    if (Led == "on")
+    if (Led == MQTT_MSG_TO_OPEN)
     {
       Serial.println("LedEncendido");
-      // digitalWrite(ledPin, HIGH);
       subirBarrera();
     }
-    else if (Led == "off")
+    else if (Led == MQTT_MSG_TO_CLOSE)
     {
       Serial.println("LedApagado");
-      // digitalWrite(ledPin, LOW);
       bajarBarrera();
     }
   }
