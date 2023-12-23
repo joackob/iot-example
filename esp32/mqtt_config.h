c867b2f14bdf9622c1b0c189e9538252d26a1dfe
Joaquin Blanco <blancojoaquin77@gmail>
2023-12-23 13:16:02 -0300

    script para configurar entornos comunes entre las apps

--- esp32/mqtt_config.h
+++ esp32/mqtt_config.h
@@ -1,7 +1,9 @@
 #ifndef MQTT_CONFIG_H
 #define MQTT_CONFIG_H
 
-#define MQTT_HOST "10.9.120.141"
+#define MQTT_HOST "mqtt://localhost:1883"
 #define MQTT_TOPIC "esp32/locker"
+#define MQTT_MSG_TO_CLOSE "close"
+#define MQTT_MSG_TO_OPEN "open"
 
-#endif // !DEBUG
+#endif //!MQTT_CONFIG_H
--- utils/config.mjs
+++ utils/config.mjs
@@ -1,6 +1,38 @@
-const config = {
+import fs from "fs";
+
+const configMQTT = {
   "mqtt-host": "mqtt://localhost:1883",
   "mqtt-topic": "esp32/locker",
   "mqtt-msg-to-close": "close",
   "mqtt-msg-to-open": "open",
 };
+
+const configESP32 = () => {
+  const mqttConfig = [
+    "#ifndef MQTT_CONFIG_H",
+    "#define MQTT_CONFIG_H",
+    "",
+    `#define MQTT_HOST "${configMQTT["mqtt-host"]}"`,
+    `#define MQTT_TOPIC "${configMQTT["mqtt-topic"]}"`,
+    `#define MQTT_MSG_TO_CLOSE_LOCKER "${configMQTT["mqtt-msg-to-close"]}"`,
+    `#define MQTT_MSG_TO_OPEN_LOCKER "${configMQTT["mqtt-msg-to-open"]}"`,
+    "",
+    `#endif //!MQTT_CONFIG_H`,
+  ];
+  fs.writeFileSync("./esp32/mqtt_config.h", mqttConfig.join("\n"));
+};
+
+const configAppToDev = () => {
+  const mqttConfig = [
+    'NODE_ENV="development"',
+    `MQTT_HOST="${configMQTT["mqtt-host"]}"`,
+    `MQTT_TOPIC="${configMQTT["mqtt-topic"]}"`,
+    `MQTT_MSG_TO_CLOSE_LOCKER="${configMQTT["mqtt-msg-to-close"]}"`,
+    `MQTT_MSG_TO_OPEN_LOCKER="${configMQTT["mqtt-msg-to-open"]}"`,
+  ];
+
+  fs.writeFileSync("./app/.env", mqttConfig.join("\n"));
+};
+
+configESP32();
+configAppToDev();
