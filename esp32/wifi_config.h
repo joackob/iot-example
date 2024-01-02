07c70a69cf3902d1dc92adf988389629fd2a4f59
Joaquin Blanco <blancojoaquin77@gmail.com>
2024-01-02 11:39:54 -0300

    eliminando pass and ssid de mi wifi

--- esp32/wifi_config.h
+++ esp32/wifi_config.h
@@ -1,7 +1,7 @@
 #ifndef WIFI_CONFIG_H
 #define WIFI_CONFIG_H
 
-#define WIFI_SSID "Telecentro-2a38"
-#define WIFI_PASS "CJNYUGZZKTNZ"
+#define WIFI_SSID "wifi-ssid"
+#define WIFI_PASS "wifi-pass"
 
-#endif // !WIFI_CONFIG_H
+#endif  // !WIFI_CONFIG_H
